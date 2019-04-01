600291fae9bd4f6957121d4073bcfddab5124aee
Saingit <sainete42@gmail.com>
2019-03-31 22:42:58 -0500

    Cambios con el registro de cursos

--- src/app.js
+++ src/app.js
@@ -12,7 +12,6 @@ const dirNode_modules = path.join(__dirname , '../node_modules')
 app.use('/css', express.static(dirNode_modules + '/mdbootstrap/css'));
 app.use('/js', express.static(dirNode_modules + '/mdbootstrap/js'));
 
-
 const directoriopublico = path.join(__dirname, '../public' );
 const directoriopartials = path.join(__dirname, '../partials' );
 hbs.registerPartials(directoriopartials);
@@ -23,14 +22,41 @@ app.set ('view engine', 'hbs');
 
 // VISTAS DE USUARIOS
 //página de inicio - login
+
 app.get('/', (req, res) =>{
   res.render('index')
 });
 
-// página de registro de usuario
-app.get('/registro', (req, res) =>{
-	res.render('registro')
+
+
+// Al hacer un registro o hacer un login me envia a esta vista
+//pagina de Home
+
+
+// Página de lista de usuarios
+app.get('/usuarios', (req, res) =>{
+	res.render('usuarios')
 });
+
+// página de registro de usuario
+// app.get('/registro', (req, res) =>{
+// 	res.render('registro'),
+// 	res.redirect('/home')
+// });
+
+
+
+app.get('/home',(req,res) => {
+	res.render('home')
+});
+
+
+// VISTAS DE CURSOS
+// Página de listado de cursos y registro
+app.get('/cursos',(req, res) =>{
+  res.render('cursos')
+});
+
 
 //pagina de registro
 app.post('/validaLogin',(req,res) => {
@@ -40,32 +66,6 @@ app.post('/validaLogin',(req,res) => {
 	});
 });
 
-// Al hacer un registro o hacer un login me envia a esta vista
-//pagina de Home
-app.post('/home',(req,res) => {
-	res.render('home')
-});
-
-
-// Página de lista de usuarios
-app.get('/usuarios', (req, res) =>{
-	res.render('usuarios')
-});
-// VISTAS DE USUARIOS FIN
-
-
-// luego de hacer el registro de usuario la pagina me lleva a cursos listando cursos disponibles
-// MARLON registro
-// app.get('/principal',(req,res) => {
-// 	res.render('principal')
-// })
-// FIN MARLON registro
-
-// VISTAS DE CURSOS
-// Página de listado de cursos y registro
-app.get('/cursos',(req, res) =>{
-  res.render('cursos')
-});
 
 // Página de Registro de cursos
 // Pendiente crear esta vista
@@ -76,19 +76,23 @@ app.post('/listacursos',(req,res) => {
 		descripcion : req.body.descripcion,
 		valor : req.body.valor,
 		modalidad : req.body.modalidad,
-		intensidad : req.body.intensidad,
-		estado : req.body.estado
+		intensidad : req.body.intensidad
+
 	});
+	res.redirect('/home');
 });
+
+
 // VISTAS DE CURSOS FIN
 
 
-// página de error
-// pendiente de funcionamiento
-app.get('/error', (req, res) =>{
-	res.render('error')
+app.get('*',(req,res) => {
+	res.render('error',{
+		Usuario: 'error'
+	});
 });
 
-app.listen(3000, () => {
-  console.log ('escuchando en el puerto')
+
+app.listen(3000,() => {
+    console.log("escuchando en el 3000");
 });
--- src/helpers.js
+++ src/helpers.js
@@ -7,7 +7,6 @@ const fs = require('fs');
 
 listaCursos = [];
 
-// Crear curso
 hbs.registerHelper('crear', (id, nombre, descripcion, valor, modalidad, intensidad) => {
 	listar();
 	let curso = {
@@ -15,7 +14,7 @@ hbs.registerHelper('crear', (id, nombre, descripcion, valor, modalidad, intensid
 		nombreC: nombre,
 		descripcionC: descripcion,
 		valorC: valor,
-		modalidaC: modalidad,
+		modalidadC: modalidad,
 		instensidadC:intensidad,
 		estado: "disponible"
 	};
@@ -24,8 +23,84 @@ hbs.registerHelper('crear', (id, nombre, descripcion, valor, modalidad, intensid
 		listaCursos.push(curso);
 		console.log(listaCursos);
 		guardar();
-		console.log('Usuario registrado con exito');
+		console.log('Curso registrado con exito');
 	}
 	else
-		console.log(`Ya Existe otro estudiante con ese nombre`);
+		console.log(`Ya Existe otro curso con ese ID`);
+
+
 });
+
+
+
+hbs.registerHelper('listar', () => {
+    listaCursos = require('../listaCursos.json')
+    let texto = '<table class="table table-sm table-dark">\
+	<thead>\
+	  <tr>\
+		<th scope="col">Id</th>\
+		<th scope="col">Nombre</th>\
+		<th scope="col">Descripcion</th>\
+		<th scope="col">Valor</th>\
+   	<th scope="col">Modalidad</th>\
+		<th scope="col">Intensidad</th>\
+		<th scope="col">Estado</th>\
+	  </tr>\
+	</thead>\
+	<tbody>'
+
+    listaCursos.forEach(curso => {
+		texto = `${texto}
+        '<tr>'
+        <td> ${curso.idC}</td>
+        <td> ${curso.nombreC}</td>
+        <td> ${curso.descripcionC}</td>
+		<td> ${curso.valorC}</td>
+		<td> ${curso.modalidadC}</td>
+		<td> ${curso.instensidadC}</td>
+		<td> ${curso.estado}</td>
+    </tr>`
+    });
+    texto = `${texto}</tbody></table> `
+    return texto;
+})
+
+const listar = () => {
+	try {
+	listaCursos = require('../listaCursos.json');
+	} catch (error) {
+		listaCursos = [];
+	}
+		
+	// listaCursos = JSON.parse(fs.readFileSync('/../src/listaCursos.json'))
+}
+
+
+const guardar = () => {
+	//guardamos el array en el archivo json
+	let datos = JSON.stringify(listaCursos);
+	fs.writeFile('../listaCursos.json', datos, (err) => {
+		if (err) throw (err);
+		console.log(`Archivo de cursos creado con exito`);
+	})
+}
+//funcion que guarda el usuario en el archivo .json
+
+// hbs.registerHelper('validarLogin', (cedulau, nombre) => {
+
+// 	listar();
+// 	let usu_cedula = listaCursos.find(usu => usu.cedulau == cedulau);
+// 	let usu_nombre = listaCursos.find(usu => usu.nombre == nombre);
+// 	if (!usu_cedula || !usu_nombre) {
+// 		return "El usuario con el que intenta ingresar no existe";
+// 	} else {
+	
+// 		console.log("Bienvenido");
+		 
+// 	}
+// });
+
+
+
+
+
