a22eca1922be9e9a1524c3d5ff87c5fa10865590
a-serna <alejo.tanatos@gmail.com>
2019-03-31 21:49:55 -0500

    Uniendo el trabajo de Javier

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const hbs = require ('hbs');
+const bodyParser = require ('body-parser');
 require('./helpers');
 
 // esta ruta permite tener accedo a la carpeta node modules
@@ -14,29 +15,76 @@ app.use('/js', express.static(dirNode_modules + '/mdbootstrap/js'));
 
 const directoriopublico = path.join(__dirname, '../public' );
 const directoriopartials = path.join(__dirname, '../partials' );
-app.use(express.static(directoriopublico));
 hbs.registerPartials(directoriopartials);
+app.use(express.static(directoriopublico));
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.set ('view engine', 'hbs');
 
+// VISTAS DE USUARIOS
+//página de inicio - login
 app.get('/', (req, res) =>{
   res.render('index')
 });
 
-app.get('/cursos',(req, res) =>{
-  res.render('cursos',{
-    curso: 'inglés'
-  });
-});
-
+// página de registro de usuario
 app.get('/registro', (req, res) =>{
 	res.render('registro')
 });
 
+//pagina de registro
+app.post('/validaLogin',(req,res) => {
+	res.render('validaLogin',{
+		cedulau: req.body.cedulau,
+		nombre: req.body.nombre
+	});
+});
+
+// Al hacer un registro o hacer un login me envia a esta vista
+//pagina de Home
+app.post('/home',(req,res) => {
+	res.render('home')
+});
+
+
+// Página de lista de usuarios
 app.get('/usuarios', (req, res) =>{
 	res.render('usuarios')
 });
+// VISTAS DE USUARIOS FIN
 
+
+// luego de hacer el registro de usuario la pagina me lleva a cursos listando cursos disponibles
+// MARLON registro
+// app.get('/principal',(req,res) => {
+// 	res.render('principal')
+// })
+// FIN MARLON registro
+
+// VISTAS DE CURSOS
+// Página de listado de cursos y registro
+app.get('/cursos',(req, res) =>{
+  res.render('cursos')
+});
+
+// Página de Registro de cursos
+// Pendiente crear esta vista
+app.post('/listacursos',(req,res) => {
+	res.render('listacursos',{
+		id : req.body.id,
+		nombre : req.body.nombre,
+		descripcion : req.body.descripcion,
+		valor : req.body.valor,
+		modalidad : req.body.modalidad,
+		intensidad : req.body.intensidad,
+		estado : req.body.estado
+	});
+});
+// VISTAS DE CURSOS FIN
+
+
+// página de error
+// pendiente de funcionamiento
 app.get('/error', (req, res) =>{
 	res.render('error')
 });
--- src/helpers.js
+++ src/helpers.js
@@ -1,3 +1,31 @@
 const hbs = require('hbs');
+const express = require('express');
+const app = express();
 
-//archivo para funciones
+//file system
+const fs = require('fs');
+
+listaCursos = [];
+
+// Crear curso
+hbs.registerHelper('crear', (id, nombre, descripcion, valor, modalidad, intensidad) => {
+	listar();
+	let curso = {
+		idC: id,
+		nombreC: nombre,
+		descripcionC: descripcion,
+		valorC: valor,
+		modalidaC: modalidad,
+		instensidadC:intensidad,
+		estado: "disponible"
+	};
+	let replica = listaCursos.find(ced => ced.idC == id)
+	if (!replica) {
+		listaCursos.push(curso);
+		console.log(listaCursos);
+		guardar();
+		console.log('Usuario registrado con exito');
+	}
+	else
+		console.log(`Ya Existe otro estudiante con ese nombre`);
+});
